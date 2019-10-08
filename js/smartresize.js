ee53560c653e8367a1d8888f63ca88bedf132183
wkaanig <15670813638@163.com>
2019-10-08 18:23:14 +0800

    Site updated: 2019-10-08 18:23:01

---
+++ js/smartresize.js
@@ -0,0 +1,3 @@
+// build time:Tue Oct 08 2019 18:22:54 GMT+0800 (GMT+08:00)
++function(i,e){var n=function(i,e,n){var r;return function t(){var t=this,u=arguments;function f(){if(!n){i.apply(t,u)}r=null}if(r){clearTimeout(r)}else if(n){i.apply(t,u)}r=setTimeout(f,e||100)}};jQuery.fn[e]=function(i){return i?this.bind("resize",n(i)):this.trigger(e)}}(jQuery,"smartresize");
+//rebuild by neat 
