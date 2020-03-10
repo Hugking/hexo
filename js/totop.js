a10b4fc23702c70a25da8af3dfc93ccd9c205274
hugking <15670813638@163.com>
2020-03-10 22:02:55 +0800

    Site updated: 2020-03-10 22:02:40

--- js/totop.js
+++ js/totop.js
@@ -1,3 +1,3 @@
-// build time:Tue Oct 08 2019 18:32:41 GMT+0800 (GMT+08:00)
+// build time:Tue Mar 10 2020 22:01:07 GMT+0800 (GMT+08:00)
 $(window).scroll(function(){$(window).scrollTop()>500?$("#rocket").addClass("show"):$("#rocket").removeClass("show")});$("#rocket").click(function(){$("#rocket").addClass("launch");$("html, body").animate({scrollTop:0},500,function(){$("#rocket").removeClass("show launch")});return false});
 //rebuild by neat 
