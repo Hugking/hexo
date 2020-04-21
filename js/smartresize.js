e4a656c5c5cf3168b63ac6c4970d8040f11a6794
hugking <15670813638@163.com>
2020-04-22 00:59:37 +0800

    Site updated: 2020-04-22 00:59:32

--- js/smartresize.js
+++
@@ -1,3 +0,0 @@
-// build time:Fri Apr 17 2020 23:25:56 GMT+0800 (GMT+08:00)
-+function(i,e){var n=function(i,e,n){var r;return function t(){var t=this,u=arguments;function f(){if(!n){i.apply(t,u)}r=null}if(r){clearTimeout(r)}else if(n){i.apply(t,u)}r=setTimeout(f,e||100)}};jQuery.fn[e]=function(i){return i?this.bind("resize",n(i)):this.trigger(e)}}(jQuery,"smartresize");
-//rebuild by neat 
--- js/totop.js
+++
@@ -1,3 +0,0 @@
-// build time:Fri Apr 17 2020 23:25:56 GMT+0800 (GMT+08:00)
-$(window).scroll(function(){$(window).scrollTop()>500?$("#rocket").addClass("show"):$("#rocket").removeClass("show")});$("#rocket").click(function(){$("#rocket").addClass("launch");$("html, body").animate({scrollTop:0},500,function(){$("#rocket").removeClass("show launch")});return false});
-//rebuild by neat 
